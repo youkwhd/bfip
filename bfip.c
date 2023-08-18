d045be3a2c1862eb99531ad8355a96ec2e7d0fc8
youkwhd <lolywk@tutanota.com>
2023-08-18 15:13:03 +0700

    chore: accept cmd line arguments

--- bfip.c
+++ bfip.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#define MEMB_INITIAL_SIZE 256
-
 #include "memb.h"
 
-int main(void)
+#define MEMB_INITIAL_SIZE 256
+
+int main(int argc, char **argv)
 {
     memb_t memb;
     memb_init(&memb, MEMB_INITIAL_SIZE);
