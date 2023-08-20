1f558b870baac90421ec57079881b2ab29389703
youkwhd <lolywk@tutanota.com>
2023-08-20 20:13:33 +0700

    feat: add read until newline

--- io.c
+++ io.c
@@ -4,12 +4,43 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void io_read_until_newline(buf_t *buf, FILE *fp)
+/* TODO:
+ */
+void io_write(io_fd_t fd)
 {
-    char ch;
+    (void) fd;
+}
 
-    while ((ch = fgetc(fp)) != '\n') {
-        buf_push_byte(buf, ch);
+char io_read_char(io_fd_t fd)
+{
+    /* since you will need to append newline
+     * read it along with char
+     *
+     * otherwise next io_read_until_newline() 
+     * will encounter \n on the first read
+     */
+    char buf[2];
+    read(fd, buf, 2);
+    return *buf;
+}
+
+void io_read_until_newline(buf_t *buf, io_fd_t fd)
+{
+    int n_bytes = 0;
+
+    while (buf_peek(buf) != '\n') {
+        if (buf->ptr++ >= (int)buf->len) {
+            buf_grow_size(buf);
+        }
+
+        n_bytes = read(fd, buf->content + buf->ptr, 1);
+        if (n_bytes <= 0) {
+            break;
+        }
+    }
+
+    if (buf_peek(buf) == '\n') {
+        buf_pop(buf);
     }
 
     buf_push_byte(buf, '\0');
--- io.h
+++ io.h
@@ -12,7 +12,12 @@
 
 typedef int io_fd_t;
 
-void io_read_until_newline(buf_t *buf, FILE *fp);
+#define IO_STDIN 0
+#define IO_STDOUT 1
+#define IO_STDERR 2
+
+char io_read_char(io_fd_t fd);
+void io_read_until_newline(buf_t *buf, io_fd_t fd);
 bool io_read(buf_t *buf, io_fd_t fd);
 bool io_close(io_fd_t fd);
 
